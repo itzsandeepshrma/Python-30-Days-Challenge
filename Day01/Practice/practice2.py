17023e56e666d66a5ea11144e1c2f5d221c99326
𝚂αɴɖεερ 𝚂ʜαяɱα <sandeepshrmadev@gmail.com>
2025-03-06 23:31:50 +0530

    Delete Day01/Practice directory

--- Day01/Practice/practice2.py
+++
@@ -1,8 +0,0 @@
-"""Q.3 Install an External Module use it to perform an operation on their interesy"""
-  
-# External Module 
-
-import pyttsx3
-engine = pyttsx3.init()
-engine.say("Hey its me Sandeep Sharma How are you, Nice to meet you buddy")
-engine.runAndWait()
