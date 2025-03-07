12b8ab77b429dea52b9fbd63fba735c93aa6a3c2
𝚂αɴɖεερ 𝚂ʜαяɱα <sandeepshrmadev@gmail.com>
2025-03-07 16:38:43 +0530

    Delete Day03 directory

--- Day03/escape_seq.py
+++
@@ -1,4 +0,0 @@
-a = 'Sandeep is a good boy\nbut not a bad \'boy\''
-
-
-print(a)
--- Day03/str_functions.py
+++
@@ -1,6 +0,0 @@
-name = "sandeep"
-
-print(len(name))
-print(name.endswith("eep"))
-print(name.startswith("San"))
-print(name.capitalize())
