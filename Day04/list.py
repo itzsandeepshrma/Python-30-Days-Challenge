12074559ae24b4a2cb5079b9ce90fc28362b7f68
𝚂αɴɖεερ 𝚂ʜαяɱα <sandeepshrmadev@gmail.com>
2025-03-07 16:41:58 +0530

    Delete Day04 directory

--- Day04/list.py
+++
@@ -1,7 +0,0 @@
-friends = ["Banana", "Orange", 5, 345.06, False, "Sandeep", "Khushi"]
-
-print(friends[0])
-friends[0] = "Orange" # Unlike Strings lists are mutable
-
-print(friends[0])
-print(friends[1:4])
--- Day04/tuple.py
+++
@@ -1,3 +0,0 @@
-a = (1,45,342,3424,False, "Rohan", "Sandeep")
-print(a)
-print(type(a))
