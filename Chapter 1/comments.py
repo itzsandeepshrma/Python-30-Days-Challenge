0a28f3b32bf2edd4a193a02937bd61621f2c04bf
𝚂αɴɖεερ 𝚂ʜαяɱα <sandeepshrmaxcoder@gmail.com>
2025-03-26 20:39:50 +0530

    Create comments.py okay

---
+++ Chapter 1/comments.py
@@ -0,0 +1,12 @@
+"""
+This is a multi-line comment.
+Used to explain complex code.
+"""
+
+def greet(name):
+    """
+    This function greets the user with a personalized message.
+    """
+    return f"Hello, {name}!"
+
+print(greet("Team Japanese"))
