71bd16ddbad01716d3980a1fcaf2ae9fa2fe31e5
𝚂αɴɖεερ 𝚂ʜαяɱα <sandeepshrmaxcoder@gmail.com>
2025-04-10 11:31:23 +0530

    Update input.py

--- Chapter 2/input.py
+++ Chapter 2/input.py
@@ -2,6 +2,9 @@ a = int(input("Enter number 1: "))
 
 b = int(input("Enter number 2: "))
 
+c = int(input("Enter number 3: "))
+
 print("Number a is: ", a)
 print("Number b is: ", b) 
-print("Sum is ", a + b)
+print("Number c is: ", c)
+print("Sum is ", a + b + c)
