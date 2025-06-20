e75c9487df5327d4e1126fe7e7cb4b6871142e10
sᴧɴᴅᴇᴇᴘ sʜᴧʀᴍᴧ <sandeepshrmaxcoder@gmail.com>
2025-06-20 12:43:31 +0530

    Update problem1.py

--- Chapter 1/Practice/problem1.py
+++ Chapter 1/Practice/problem1.py
@@ -1,10 +1,4 @@
-print('''demon slayer pain
-feel free like an eagle
-too not ever yield back
-is always a promise to this self
-from pain to pure strength
-for a slayer fiercely waiting there I
-to even slice an enormous boulder
-that gets in my way
-so don't you dare
-come close to the ones I care''')
+print("""Beneath the moon, where silence grows,
+A lone soul walks where no one knows.
+With scars unseen and eyes that burn,
+He waits for dawn that won't return.""")
