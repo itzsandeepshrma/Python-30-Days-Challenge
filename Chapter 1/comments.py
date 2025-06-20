53113cd2d985c09c95ae99510896a0d58ced4446
sᴧɴᴅᴇᴇᴘ sʜᴧʀᴍᴧ <sandeepshrmaxcoder@gmail.com>
2025-06-20 12:34:00 +0530

    Update comments.py

--- Chapter 1/comments.py
+++ Chapter 1/comments.py
@@ -1,17 +1,19 @@
-# Single Line Comments 
+"""Single Line Comments"""
 
-'''In the shadows, demons hide,  
-  Tanjiro walks with Nezuko by his side.  
-  With sword of sun and heart so kind,  
-  He leaves no fallen soul behind.
+# In the shadows, demons hide,  
+# Tanjiro walks with Nezuko by his side.  
+# With sword of sun and heart so kind,  
+# He leaves no fallen soul behind.
 
-– Tanjiro Kamado'''
+# – Tanjiro Kamado
 
-# Mult Line Comments 
+'''Multi-line Comments'''
 
-"""Thunder cracks and fear takes flight,  
-  Zenitsu strikes in a flash of light.  
-  Though he trembles, though he shakes,  
-  One breath of thunder is all it takes.
+"""
+Thunder cracks and fear takes flight,  
+Zenitsu strikes in a flash of light.  
+Though he trembles, though he shakes,  
+One breath of thunder is all it takes.
 
-– Zenitsu Agatsuma"""
+– Zenitsu Agatsuma
+"""
