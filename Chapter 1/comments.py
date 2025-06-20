2c2aa65742e6f6478d4394e0c0235bebcf72a6e6
sᴧɴᴅᴇᴇᴘ sʜᴧʀᴍᴧ <sandeepshrmaxcoder@gmail.com>
2025-06-20 12:29:48 +0530

    Update comments.py

--- Chapter 1/comments.py
+++ Chapter 1/comments.py
@@ -1,4 +1,17 @@
-"""
-This is a multi-line comment.
-Used to explain complex code.
-"""
+# Single Line Comments 
+
+'''In the shadows, demons hide,  
+  Tanjiro walks with Nezuko by his side.  
+  With sword of sun and heart so kind,  
+  He leaves no fallen soul behind.
+
+– Tanjiro Kamado'''
+
+# Mult Line Comments 
+
+"""Thunder cracks and fear takes flight,  
+  Zenitsu strikes in a flash of light.  
+  Though he trembles, though he shakes,  
+  One breath of thunder is all it takes.
+
+– Zenitsu Agatsuma"""
