858af76543fd50f5628cde909cb944a93f564bf7
sᴧɴᴅᴇᴇᴘ sʜᴧʀᴍᴧ <sandeepshrmaxcoder@gmail.com>
2025-06-20 12:24:16 +0530

    Update comments.py

--- Chapter 1/comments.py
+++ Chapter 1/comments.py
@@ -2,11 +2,3 @@
 This is a multi-line comment.
 Used to explain complex code.
 """
-
-def greet(name):
-    """
-    This function greets the user with a personalized message.
-    """
-    return f"Hello, {name}!"
-
-print(greet("Team Japanese"))
